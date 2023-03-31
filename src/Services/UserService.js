3fafb190fd5f44908c35effb67061e2b188eccdf
connectaarka <connectaarka@gmail.com>
2023-03-31 22:21:32 +0530

    service changes

---
+++ src/Services/UserService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import Config from "../Constants/Config";
+
+
+const baseUrl = Config.baseUrl;
+
+export const loginUser = (url,data) => {
+    return axios.post(baseUrl + url,data);
+}
+export const registerUSer = (url,data) => {
+    return axios.post(baseUrl + url,data);
+}
+export const getUserDetails = (url) => {
+    return axios.get(baseUrl + url);
+}
+export const getAllUsers = (url) => {
+    return axios.get(baseUrl + url);
+}
