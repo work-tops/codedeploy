b4531fe43bdef3d1c41e49ae278e1cd5bec04ba7
work-tops <testworktops@gmail.com>
2023-04-01 18:53:55 +0530

    commit 7

--- src/Components/Product/AiProductDetails.js
+++ src/Components/Product/AiProductDetails.js
@@ -5,116 +5,103 @@ import AiMenu from "../Menubar/AiMenu";
 function AiProductDetails() {
     return (
         <>
-<<<<<<< HEAD
-            <div>
-=======
+
             <div className="">
->>>>>>> ef9035a00b9b1b54011090252c3253d9547c1a13
                 <div className="row">
                     <div className="col-2">
                         <AiMenu />
                     </div>
                     <div className="col-10">
                         <div className="ai-product-div">
+                            <AiHeader />
                             <div>
-<<<<<<< HEAD
-                                <AiHeader />
-=======
-                                {/* <AiHeader /> */}
->>>>>>> ef9035a00b9b1b54011090252c3253d9547c1a13
-                            </div>
-                            <div>
-                            <div className="ms-3">
+                                <div>
+                                    <div className="ms-3">
                                         <p className="ai-sub-title">products/edit product</p>
                                         <h4>Add Product</h4>
                                         <p className="ai-title-desc">Here You Can add products to your profile</p>
-                                        </div>
-                                <div className="product-div">
-                                    <div className="Add-Product">
-<<<<<<< HEAD
-
-=======
-                                       
->>>>>>> ef9035a00b9b1b54011090252c3253d9547c1a13
-                                        <form>
-                                            <label>Product Name</label>
-                                            <input id="aipro-name" type='text' />
-                                            <br></br>
-                                            <span className="category">Category</span> <span className="seller-email">Seller Email</span>
-                                            <br></br>
-                                            <input id="aipro-category" type='text' />
-                                            <input id="aipro-email" type='email' />
-                                            <br/>
-                                            <label>Description</label>
-                                            <br/>
-                                            <textarea id="aipro-description" className="ai-product-description"></textarea>
-                                            <br/>
-                                            <label>Product Tag</label>
-                                            <br/>
-                                            <input className="ai-product-tag" type='text'></input>
-                                            <br></br>
-                                            <br></br>
-                                            <input id="aipro-checkbox1" type='checkbox' /><span className="chc-span">Shipping Requires</span>
-                                            <input id="aipro-checkbox2" type='checkbox' /><span className="chc-span">Charge Taxes on this product</span>
-                                            <br></br>
-                                            <br></br>
-                                            <span className="category">Price</span> <span className="compareprice">Compare Price</span>
-                                            <br></br>
-                                            <input id="aipro-category" type='text' />
-                                            <input id="aipro-email" type='email' />
-                                            <br></br>
-                                            <br></br>
-                                            <span className="category">Handling Charges</span> <span className="salesprice">Sales Price</span>
-                                            <br></br>
-                                            <input id="aipro-category" type='text' />
-                                            <input id="aipro-email" type='email' />
-                                            <br></br>
-                                            <label>SKU</label>
-                                            <input id="aipro-sku" type='text' />
-                                            <label>Barcode</label>
-                                            <input id="aipro-barcode" type='text' />
-                                            <label>Minimum Purchase Quality</label>
-                                            <input id="aipro-min-purchase_quality" type='text' />
-                                            <label>Quantity</label>
-                                            <input id="aipro-quantity" type='text' />
-                                            <br></br>
-                                            <input id="aipro-checkbox" type='checkbox' /><span className="chc-span">Track This Product Inventory</span>
-                                            <button id="aipro-addvariant"><i className="ri-add-line"></i>Add Variant</button>
-                                            <label>Return Policy</label>
-                                            <br/>
-                                            <textarea id="aipro-returnpolicy"></textarea>
-                                            <input type='button' id="add-product-btn" value='Add Product' />
-                                        </form>
                                     </div>
-                                    <div className="Add-Product-Images">
-                                        <p className="ai-pro-title">Product Images</p>
-                                        <div className="ai-image-drag">
-                                            <i className="ai-img-icon ri-image-fill"></i>
-                                            <small className="chose-file">No File Chosen</small>
-                                        </div>
-                                        <button className="img-upload-btn">Upload Images</button>
-                                        <p className="pro-sub-title">Product Handle and Metafields</p>
-<<<<<<< HEAD
-                                        <span className="label">Product handle</span>
-=======
-                                        <label>Product handle</label>
->>>>>>> ef9035a00b9b1b54011090252c3253d9547c1a13
-                                        <input id="ai-pro-handle" type='text' />
-                                        <p className="pro-sub-title">Product Meta Fields</p>
-                                        <p className="ai-title-desc">Title tag meta field</p>
-                                        <input id="ai-pro-handle" type='text' />
-                                        <p className="ai-title-desc">Descrption tag meta field</p>
-                                        <input id="ai-pro-handle" type='text' />
-                                    </div>
+                                    <div className="product-div">
+                                        <div className="Add-Product">
+                                            <form>
+                                                <label>Product Name</label>
+                                                <input id="aipro-name" type='text' />
+                                                <br></br>
+                                                <span className="category">Category</span> <span className="seller-email">Seller Email</span>
+                                                <br></br>
+                                                <input id="aipro-category" type='text' />
+                                                <input id="aipro-email" type='email' />
+                                                <br />
+                                                <label>Description</label>
+                                                <br />
+                                                <textarea id="aipro-description" className="ai-product-description"></textarea>
+                                                <br />
+                                                <label>Product Tag</label>
+                                                <br />
+                                                <input className="ai-product-tag" type='text'></input>
+                                                <br></br>
+                                                <br></br>
+                                                <input id="aipro-checkbox1" type='checkbox' /><span className="chc-span">Shipping Requires</span>
+                                                <input id="aipro-checkbox2" type='checkbox' /><span className="chc-span">Charge Taxes on this product</span>
+                                                <br></br>
+                                                <br></br>
+                                                <span className="category">Price</span> <span className="compareprice">Compare Price</span>
+                                                <br></br>
+                                                <input id="aipro-category" type='text' />
+                                                <input id="aipro-email" type='email' />
+                                                <br></br>
+                                                <br></br>
+                                                <span className="category">Handling Charges</span> <span className="salesprice">Sales Price</span>
+                                                <br></br>
+                                                <input id="aipro-category" type='text' />
+                                                <input id="aipro-email" type='email' />
+                                                <br></br>
+                                                <label>SKU</label>
+                                                <input id="aipro-sku" type='text' />
+                                                <label>Barcode</label>
+                                                <input id="aipro-barcode" type='text' />
+                                                <label>Minimum Purchase Quality</label>
+                                                <input id="aipro-min-purchase_quality" type='text' />
+                                                <label>Quantity</label>
+                                                <input id="aipro-quantity" type='text' />
+                                                <br></br>
+                                                <input id="aipro-checkbox" type='checkbox' /><span className="chc-span">Track This Product Inventory</span>
+                                                <button id="aipro-addvariant"><i className="ri-add-line"></i>Add Variant</button>
+                                                <label>Return Policy</label>
+                                                <br />
+                                                <textarea id="aipro-returnpolicy"></textarea>
+                                                <input type='button' id="add-product-btn" value='Add Product' />
+                                            </form>
+                                        </div >
+                                        <div className="Add-Product-Images">
+                                            <p className="ai-pro-title">Product Images</p>
+                                            <div className="ai-image-drag">
+                                                <i className="ai-img-icon ri-image-fill"></i>
+                                                <small className="chose-file">No File Chosen</small>
+                                            </div>
+                                            <button className="img-upload-btn">Upload Images</button>
+                                            <p className="pro-sub-title">Product Handle and Metafields</p>
 
-                                </div>
-                            </div>
-                        </div>
+                                            <span className="label">Product handle</span>
+                                            <label>Product handle</label>
 
-                    </div>
-                </div>
+                                            <input id="ai-pro-handle" type='text' />
+                                            <p className="pro-sub-title">Product Meta Fields</p>
+                                            <p className="ai-title-desc">Title tag meta field</p>
+                                            <input id="ai-pro-handle" type='text' />
+                                            <p className="ai-title-desc">Descrption tag meta field</p>
+                                            <input id="ai-pro-handle" type='text' />
+                                        </div >
+
+                                    </div >
+                                </div >
+                            </div >
+
+                        </div >
+                    </div >
+                </div >
             </div>
         </>
     )
-    }
+}
 export default AiProductDetails
--- src/Components/Seller/AllSeller.js
+++ src/Components/Seller/AllSeller.js
@@ -45,7 +45,6 @@ function AllSeller() {
                         {' '}
                         List
                     </p>
-<<<<<<< HEAD
                     <div>
                         <div className="shadows">
                             <table className="allproducts-table">
@@ -135,105 +134,6 @@ function AllSeller() {
                                 <button className="next-btn"><i className="fa-solid fa-chevron-right"></i></button>
                             </div>
                         </div>
-                        {/* <table >
-=======
-                    <div className="shadow  p-3 mb-5 bg-body rounded">
-                        <table className="allsellers-table" >
->>>>>>> ef9035a00b9b1b54011090252c3253d9547c1a13
-                            <tr className="product-heading ">
-                                <td id="td" className="rounded-start"><input type='checkbox'  /></td>
-                                <td id="td">Seller ID</td>
-                                <td id="td">Seller Profile</td>
-                                <td id="td">Name</td>
-                                <td id="td">Company Name</td>
-                                <td id="td">Verfication</td>
-                                <td id="td">Date</td>
-                                <td id="td">Status</td>
-                                <td id="td" className="rounded-end">Option</td>
-                            </tr>
-                            <tr>
-                                <td id="td"><input type='checkbox' /></td>
-                                <td id="td">2456781</td>
-                                <td id="td"><img src={pom} alt="pro-thumb" className="img-curve" /></td>
-                                <td id="td">Emeka Warehouse</td>
-                                <td id="td">Granite Slab UK</td>
-                                <td id="td"><span className="pro-status-approved">Verified</span></td>
-                                <td id="td">31 Mar 2023.</td>
-                                <td id="td"><span className="pro-status-approved">Approved</span></td>
-                                <td id="td"><i className="fa-solid fa-ellipsis"></i></td>
-                            </tr>
-                            <tr>
-                                <td id="td"><input type='checkbox' /></td>
-                                <td id="td">2456781</td>
-                                <td id="td"><img src={pom} alt="pro-thumb" className="img-curve" /></td>
-                                <td id="td">Emeka Warehouse</td>
-                                <td id="td">Granite Slab UK</td>
-                                <td id="td"><span className="pro-status-approval_pending">Not Verified</span></td>
-                                <td id="td">31 Mar 2023.</td>
-                                <td id="td"><span className="pro-status-approval_pending">Approval Pending</span></td>
-                                <td id="td"><i className="fa-solid fa-ellipsis"></i></td>
-                            </tr>
-                            <tr>
-                                <td id="td"><input type='checkbox' /></td>
-                                <td id="td">2456781</td>
-                                <td id="td"><img src={pom} alt="pro-thumb" className="img-curve" /></td>
-                                <td id="td">Emeka Warehouse</td>
-                                <td id="td">Granite Slab UK</td>
-                                <td id="td"><span className="pro-status-approved">Verified</span></td>
-                                <td id="td">31 Mar 2023.</td>
-                                <td id="td"><span className="pro-status-disabled">Disabled</span></td>
-                                <td id="td"><i className="fa-solid fa-ellipsis"></i></td>
-                            </tr>
-                            <tr>
-                                <td id="td"><input type='checkbox' /></td>
-                                <td id="td">2456781</td>
-                                <td id="td"><img src={pom} alt="pro-thumb" className="img-curve" /></td>
-                                <td id="td">Emeka Warehouse</td>
-                                <td id="td">Granite Slab UK</td>
-                                <td id="td"><span className="pro-status-approved">Verified</span></td>
-                                <td id="td">31 Mar 2023.</td>
-                                <td id="td"><span className="pro-status-approved">Approved</span></td>
-                                <td id="td"><i className="fa-solid fa-ellipsis"></i></td>
-                            </tr>
-                            <tr>
-                                <td id="td"><input type='checkbox' /></td>
-                                <td id="td">2456781</td>
-                                <td id="td"><img src={pom} alt="pro-thumb" className="img-curve" /></td>
-                                <td id="td">Emeka Warehouse</td>
-                                <td id="td">Granite Slab UK</td>
-                                <td id="td"><span className="pro-status-approved">Verified</span></td>
-                                <td id="td">31 Mar 2023.</td>
-                                <td id="td"><span className="pro-status-approved">Approved</span></td>
-                                <td id="td"><i className="fa-solid fa-ellipsis"></i></td>
-                            </tr>
-                            <tr>
-                                <td id="td"><input type='checkbox' /></td>
-                                <td id="td">2456781</td>
-                                <td id="td"><img src={pom} alt="pro-thumb" className="img-curve" /></td>
-                                <td id="td">Emeka Warehouse</td>
-                                <td id="td">Granite Slab UK</td>
-                                <td id="td"><span className="pro-status-approved">Verified</span></td>
-                                <td id="td">31 Mar 2023.</td>
-                                <td id="td"><span className="pro-status-approved">Approved</span></td>
-                                <td id="td"><i className="fa-solid fa-ellipsis"></i></td>
-                            </tr>
-                        </table> */}
-
-                        {/* <span className="showing-tag-name">Showing 1-30 List</span>
-                        <div className="all-pro-pagination ">
-                            <button className="back-btn shadow bg-body rounded"><i className="fa-solid fa-chevron-left"></i></button>
-                            <button className="shadow bg-body rounded">1</button>
-                            <button className="shadow bg-body rounded">2</button>
-                            <button className="shadow bg-body rounded">3</button>
-                            <button className="shadow bg-body rounded"><i className="fa-solid fa-ellipsis"></i></button>
-                            <button className="shadow bg-body rounded">25</button>
-<<<<<<< HEAD
-                            <button className="next-btn"><i class="fa-solid fa-chevron-right"></i></button>
-                        </div> */}
-=======
-                            <button className="next-btn"><i className="fa-solid fa-chevron-right"></i></button>
-                        </div>
->>>>>>> ef9035a00b9b1b54011090252c3253d9547c1a13
                     </div>
                 </div>
             </div>
