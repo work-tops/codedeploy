023cfca089705f0a5740aa98586f20beb412e5d0
connectaarka <connectaarka@gmail.com>
2023-04-13 09:19:11 +0530

    new changes

--- src/Components/Commission/CategoryCommission.js
+++ src/Components/Commission/CategoryCommission.js
@@ -1,8 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AiMenu from "../Menubar/AiMenu";
 import AiHeader from "../Header/AiHeader";
+import { createData, getAllData } from "../../Services/ProxyService";
+import toast, { Toaster } from 'react-hot-toast';
 
 function CategoryCommission() {
+
+    const [form, setform] = useState([]);
+    const [form1, setForm1] = useState({ update_old_values: false });
+    const [category, setcategory] = useState([]);
+    const [service, setservice] = useState([]);
+
+    const handlechange = (e) => {
+        const myData = { ...form };
+        myData[e.target.name] = e.target.value;
+        setform(myData)
+    }
+
+    const handlechange1 = (e) => {
+        setForm1({
+            ...form1,
+            [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value
+        });
+    };
+
+    const Productsdata = async () => {
+        const response = await getAllData("master/product_types")
+        setcategory(response.data.master[0].data)
+    }
+
+    const Servicedata = async () => {
+        const response = await getAllData("master/service_category")
+        setservice(response.data.master[0].data)
+    }
+
+    useEffect(() => {
+        Productsdata()
+        Servicedata()
+    }, [])
+
+    const AddCCcommission = async () => {
+        const GBcommdata = {
+            commission_type: form.commission_type,
+            commission_category: 3,
+            email: "",
+            first_commission: form.first_commission,
+            second_commission: form.second_commission,
+            category: form.category,
+            option: form.option,
+            status: true,
+            update_old_values: form1.update_old_values,
+            created_by: "1",
+        }
+        const response = await createData("commission/new", GBcommdata)
+        if (response.status === 201) {
+            toast.success('Successfully Category Commission Added')
+            setform("")
+            setForm1("")
+            cleardata()
+        } else {
+            toast.error('Something went wrong')
+        }
+        console.log(response)
+    }
+
+    const formsubmit = (e) => {
+        e.preventDefault()
+        AddCCcommission();
+        setForm1("")
+    }
+
+    const cleardata = () => {
+        setform({
+            commission_type: "",
+            email: "",
+            first_commission: "",
+            second_commission: "",
+            category: "",
+            option: "",
+            update_old_values: false,
+
+        })
+    }
+
     return (
         <>
             <div className="row">
@@ -17,7 +97,7 @@ function CategoryCommission() {
                         <div className="col-12 page-bg">
                             <div>
                                 <p className="ms-3 ai-sub-title">Commission / Category Commission</p>
-                                <h4  className="ms-3">Category Commission</h4>
+                                <h4 className="ms-3">Category Commission</h4>
                                 <p className="ms-3 ai-tit-desc">Here are setting for Category Commission</p>
                                 <div className="imp-note">
                                     <p className="not-tit">Note:</p>
@@ -38,76 +118,92 @@ function CategoryCommission() {
                                         </li>
                                     </ul>
                                 </div>
-                                <div className="global-com-setting-div">
+                               <form onSubmit={(e) => { formsubmit(e) }}>
+                               <div className="global-com-setting-div">
                                     <p className="global-com-setting-heading">Category Commission</p>
                                     <p className="global-desc">Here are the setting for category commission</p>
-                                    <span className="category">Select Category Type</span> 
-                                    <span className="category-service">Select Product Category</span> 
-                                    <span className="category-service-1">Select Service Category</span>
+                                    <span className="category">Select Category Type</span>
                                     <br></br>
-                                    <select id="aipro-service-comm">
-                                        <option>Choose the Option</option>
-                                        <option>Products</option>
-                                        <option>Services</option>
+                                    <select required name="category" onChange={(e) => { handlechange(e) }} id="aipro-service-comm">
+                                        <option value="">Choose the Option</option>
+                                        <option value="Products">Products</option>
+                                        <option value="Services">Services</option>
                                     </select>
-                                    <select id="aipro-service-comm">
-                                        <option>Choose the Option</option>
-                                        <option>Granite</option>
-                                        <option>Marble</option>
-                                        <option>Quartz</option>
-                                        <option>Tiles</option>
-                                        <option>Quartzite</option>
-                                    </select>
-                                    <select id="aipro-service-comm">
-                                        <option>Choose the Option</option>
-                                        <option>Cleaning</option>
-                                        <option>Cabinets </option>
-                                        <option>fixing</option>
-                                        <option> Worktops  Installation</option>
-                                        <option>Loft Installation</option>
-                                        <option>Solar Panels Installation</option>
-                                        <option>Electrical Help</option>
-                                    </select>
+                                    {form.category == "Products" ? (
+                                        <>
+                                            <span className="category-service">Select Product Category</span>
+                                            <select  value={form.option} required name="option" onChange={(e) => { handlechange(e) }} id="aipro-service-comm">
+                                                <option value="">Choose the Option</option>
+                                                {category.map((data, key) => (
+                                                    <option key={key} value={data.name}>{data.name}</option>
+                                                ))}
+                                            </select>
+                                        </>
+
+                                    ) : ( "" )}
+
+                                    {form.category == "Services" ? (
+                                        <>
+                                            <span className="category-service">Select Service Category</span>
+                                            <select  value={form.option} required name="option" onChange={(e) => { handlechange(e) }} id="aipro-service-comm">
+                                                <option>Choose the Option</option>
+                                                {service.map((data, key) => (
+                                                    <option key={key} value={data.value}>{data.value}</option>
+                                                ))}
+                                            </select>
+                                        </>
+                                    ) : ""}
+
                                     <br></br>
                                     <br></br>
                                     <p className="gct">Category Commission Type</p>
-                                    <select className="cgct">
-                                        <option>Choose category commission type</option>
-                                        <option>%</option>
-                                        <option>Fixed</option>
-                                        <option>% + Fixed</option>
-                                        <option>Fixed + %</option>
+                                    <select value={form.commission_type} required name="commission_type" onChange={(e) => { handlechange(e) }} className="cgct">
+                                        <option value="">Choose category commission type</option>
+                                        <option value="%">%</option>
+                                        <option value="Fixed">Fixed</option>
+                                        <option value="% + Fixed">% + Fixed</option>
+                                        <option value="Fixed + %">Fixed + %</option>
                                     </select>
                                     <hr></hr>
-                                    <label className="label">Category Commission</label>
-                                    <input id="fgc" /><button className="per-btn-dis" disabled>%</button>
-                                    <hr></hr>
-                                    <label className="label">Category Commission</label>
-                                    <input id="fgc" /><button className="fix-btn-dis" disabled>FIXED</button>
-                                    <hr></hr>
-                                    <label className="label">Category Commission</label>
-                                    <input id="fgc" /><button className="per-btn-dis" disabled>%</button>
-                                    <label className="label">Second Category Commission</label>
-                                    <input id="fgc" /><button className="fix-btn-dis" disabled>FIXED</button>
-                                    <hr></hr>
-                                    <label className="label">Category Commission</label>
-                                    <input id="fgc" /><button className="fix-btn-dis" disabled>FIXED</button>
-                                    <label className="label">Second Category Commission</label>
-                                    <input id="fgc" /><button className="per-btn-dis" disabled>%</button>
-                                    <hr></hr>
+                                    {form.commission_type == "%" || form.commission_type == "Fixed" ? (
+                                        <>
+                                            <label className="label">Category Commission</label>
+                                            <input type="number" required value={form.first_commission} id="fgc" name="first_commission" onChange={(e) => { handlechange(e) }} /><button className="per-btn-dis fix-btn-dis" disabled>{form.commission_type}</button>
+                                            <hr></hr>
+                                        </>
+                                    ) : ""}
+                                    {form.commission_type == "% + Fixed" || form.commission_type == "Fixed + %" ? (
+                                        <>
+                                            <label className="label">Category Commission</label>
+                                            <input type="number" required value={form.first_commission} name="first_commission" onChange={(e) => { handlechange(e) }} id="fgc" /><button className="per-btn-dis" disabled>%</button>
+                                            <hr></hr>
+                                            <label className="label">Second Category Commission</label>
+                                            <input type="number" id="fgc" required value={form.second_commission} name="second_commission" onChange={(e) => { handlechange(e) }} /><button className="fix-btn-dis" disabled>FIXED</button>
+                                            <hr></hr>
+                                        </>
+                                    ) : ""}
                                     <p className="upd-titl">UPDATE EARLIER PRODUCT PRICE</p>
                                     <div className="form-check form-switch">
-                                        <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                                        <br></br>
+                                        <input
+                                            name="update_old_values"
+                                            checked={form1.update_old_values}
+                                            onChange={handlechange1}
+                                            className="form-check-input"
+                                            type="checkbox"
+                                            role="switch"
+                                            id="flexSwitchCheckDefault"
+                                        />                                        <br></br>
                                         <p className="switch-des">If you enable this option , all the prices of exisiting products will change according to the given commission value</p>
-                                        <button className="create-acc-btn-1">Save Changes</button>
+                                        <button type="submit" className="create-acc-btn-1">Save Changes</button>
                                     </div>
                                 </div>
+                               </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <Toaster />
         </>
     )
 }
--- src/Components/Commission/ServiceCommission.js
+++ src/Components/Commission/ServiceCommission.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import AiMenu from "../Menubar/AiMenu";
 import AiHeader from "../Header/AiHeader";
+import { createData } from "../../Services/ProxyService";
+import toast, { Toaster } from 'react-hot-toast';
 function ServiceCommission() {
+
+    const [form, setform] = useState([]);
+    const [form1, setForm1] = useState({ update_old_values: false });
+
+    const handlechange = (e) => {
+        const myData = { ...form };
+        myData[e.target.name] = e.target.value;
+        setform(myData)
+    }
+    const handlechange1 = (e) => {
+        setForm1({
+            ...form1,
+            [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value
+        });
+    };
+    const AddSRcommission = async () => {
+        const GBcommdata = {
+            commission_type: form.commission_type,
+            commission_category: 2,
+            email: "",
+            first_commission: form.first_commission,
+            second_commission: form.second_commission,
+            category: "default",
+            option: "default",
+            status: true,
+            update_old_values: form1.update_old_values,
+            created_by: "1",
+        }
+        const response = await createData("commission/new", GBcommdata)
+        if (response.status === 201) {
+            toast.success('Successfully Service Commission Added')
+            setform("")
+            setForm1("")
+            cleardata()
+        } else {
+            toast.error('Something went wrong')
+        }
+        console.log(response)
+    }
+
+    const formsubmit = (e) => {
+        e.preventDefault()
+        AddSRcommission();
+        setForm1("")
+    }
+
+    const cleardata = () => {
+        setform({
+            commission_type: "",
+            email: "",
+            first_commission: "",
+            second_commission: "",
+            category: "",
+            option: "",
+            update_old_values: false,
+
+        })
+    }
+
     return (
         <>
             <div className="row">
@@ -37,47 +98,62 @@ function ServiceCommission() {
                                         </li>
                                     </ul>
                                 </div>
-                                <div className="global-com-setting-div">
-                                    <p className="global-com-setting-heading">Service Commission</p>
-                                    <p className="global-desc">Here are the setting for service commission</p>
-                                    <p className="gct">Service Commission Type</p>
-                                    <select className="cgct">
-                                        <option>Choose service commission type</option>
-                                        <option>%</option>
-                                        <option>Fixed</option>
-                                        <option>% + Fixed</option>
-                                        <option>Fixed + %</option>
-                                    </select>
-                                    <hr></hr>
-                                    <label className="label">Service Commission</label>
-                                    <input id="fgc" /><button className="per-btn-dis" disabled>%</button>
-                                    <hr></hr>
-                                    <label className="label">Service Commission</label>
-                                    <input id="fgc" /><button className="fix-btn-dis" disabled>FIXED</button>
-                                    <hr></hr>
-                                    <label className="label">Service Commission</label>
-                                    <input id="fgc" /><button className="per-btn-dis" disabled>%</button>
-                                    <label className="label">Second Service Commission</label>
-                                    <input id="fgc" /><button className="fix-btn-dis" disabled>FIXED</button>
-                                    <hr></hr>
-                                    <label className="label">Service Commission</label>
-                                    <input id="fgc" /><button className="fix-btn-dis" disabled>FIXED</button>
-                                    <label className="label">Second Service Commission</label>
-                                    <input id="fgc" /><button className="per-btn-dis" disabled>%</button>
-                                    <hr></hr>
-                                    <p className="upd-titl">UPDATE EARLIER PRODUCT PRICE</p>
-                                    <div className="form-check form-switch">
-                                        <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                                        <br></br>
-                                        <p className="switch-des">If you enable this option , all the prices of exisiting products will change according to the given commission value</p>
-                                        <button className="create-acc-btn-1">Save Changes</button>
+                                <form onSubmit={(e) => { formsubmit(e) }}>
+                                    <div className="global-com-setting-div">
+                                        <p className="global-com-setting-heading">Service Commission</p>
+                                        <p className="global-desc">Here are the setting for service commission</p>
+                                        <p className="gct">Service Commission Type</p>
+                                        <select value={form.commission_type} required name="commission_type" onChange={(e) => { handlechange(e) }} className="cgct">
+                                            <option value="">Choose service commission type</option>
+                                            <option value="%">%</option>
+                                            <option value="Fixed">Fixed</option>
+                                            <option value="% + Fixed">% + Fixed</option>
+                                            <option value="Fixed + %">Fixed + %</option>
+                                        </select>
+                                        <hr></hr>
+                                        {form.commission_type == "%" || form.commission_type == "Fixed" ? (
+                                            <>
+                                                <label className="label">Service Commission</label>
+                                                <input type="number" required value={form.first_commission} id="fgc" name="first_commission" onChange={(e) => { handlechange(e) }} /><button className="per-btn-dis fix-btn-dis" disabled>{form.commission_type}</button>
+                                                <hr></hr>
+                                            </>
+                                        ) : ""}
+                                        {form.commission_type == "% + Fixed" || form.commission_type == "Fixed + %" ? (
+                                            <>
+                                                <label className="label">Service Commission</label>
+                                                <input  type="number" required value={form.first_commission} name="first_commission" onChange={(e) => { handlechange(e) }} id="fgc" /><button className="per-btn-dis" disabled>%</button>
+                                                <hr></hr>
+                                                <label className="label">Second Service Commission</label>
+                                                <input  type="number" id="fgc" required value={form.second_commission} name="second_commission" onChange={(e) => { handlechange(e) }} /><button className="fix-btn-dis" disabled>FIXED</button>
+                                                <hr></hr>
+                                            </>
+                                        ) : ""}
+
+
+                                        <p className="upd-titl">UPDATE EARLIER PRODUCT PRICE</p>
+                                        <div className="form-check form-switch">
+                                            <input
+                                            
+                                                name="update_old_values"
+                                                checked={form1.update_old_values}
+                                                onChange={handlechange1}
+                                                className="form-check-input"
+                                                type="checkbox"
+                                                role="switch"
+                                                id="flexSwitchCheckDefault"
+                                            />
+                                            <br></br>
+                                            <p className="switch-des">If you enable this option , all the prices of exisiting products will change according to the given commission value</p>
+                                            <button type="submit" className="create-acc-btn-1">Save Changes</button>
+                                        </div>
                                     </div>
-                                </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <Toaster />
         </>
     )
 }
