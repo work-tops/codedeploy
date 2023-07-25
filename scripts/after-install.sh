57f4f9994aad593bef473464b055df35637c79d0
manojdesen1 <107014029+manojdesen1@users.noreply.github.com>
2023-07-25 19:07:31 +0530

    Update after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Clone the GitHub repository and place it in /dist/
-cd /var/www/
-git clone https://github.com/manojdesen1/codebuild.git .
+#cd /var/www/
+#git clone https://github.com/manojdesen1/codebuild.git .
 
 # Install Node.js app dependencies
 cd /var/www/codebuild
