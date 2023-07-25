4a6cca300a004cc60ac4f7beba7039c39cae68bf
manojdesen1 <107014029+manojdesen1@users.noreply.github.com>
2023-07-25 16:12:42 +0530

    Update before-install.sh

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Install Node.js and npm (if not already installed)
 sudo apt update
+sudo rm -rf /var/www/coudebuild/*
 # sudo apt install -y nodejs npm
-cd /var/www/html/
-sudo rm -rf *
+
+
