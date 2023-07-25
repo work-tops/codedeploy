94017cf558c3cc5a400e6cecc9b57b64a38cce18
manojdesen1 <107014029+manojdesen1@users.noreply.github.com>
2023-07-25 17:51:56 +0530

    Update before-install.sh

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Install Node.js and npm (if not already installed)
 sudo apt update
+sudo systemctl restart nginx
 pm2 stop codebuild
 sudo rm -rf /var/www/codebuild/*
 # sudo apt install -y nodejs npm
