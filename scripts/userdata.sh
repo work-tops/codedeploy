df5d6bf792d12939e388b53d09b5525224189210
work-tops <127959050+work-tops@users.noreply.github.com>
2023-07-27 10:42:10 +0530

    Create userdata.sh

---
+++ scripts/userdata.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update the package list and upgrade existing packages
+sudo apt update
+sudo apt upgrade -y
+
+# Install Nginx
+sudo apt install -y nginx
+
+# Install Node.js and npm
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt install -y nodejs
+
+# Install PM2
+sudo npm install -g pm2
+
+# Start Nginx
+sudo systemctl start nginx
+
+# Enable Nginx to start on boot
+sudo systemctl enable nginx
+
+sudo nginx -s reload
