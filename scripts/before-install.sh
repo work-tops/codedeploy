b709821e597728ba6bf6cf5142975fdbf669836b
work-tops <127959050+work-tops@users.noreply.github.com>
2023-08-07 15:14:26 +0530

    Update before-install.sh

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -2,8 +2,8 @@
 # Install Node.js and npm (if not already installed)
 sudo apt update
 sudo systemctl restart nginx
-pm2 stop codebuild
-sudo rm -rf /var/www/codebuild/*
+pm2 stop codedeploy
+sudo rm -rf /var/www/codedeploy/*
 # sudo apt install -y nodejs npm
 
 
